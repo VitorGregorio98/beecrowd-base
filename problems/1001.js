32c5ff42e3e28ad052f90b8cb47bbdecd964f2bb
Vitor Gregório <161853904+VitorGregorio98@users.noreply.github.com>
2024-04-29 22:52:16 -0300

    fiz o 1001

---
+++ problems/1001.js
@@ -0,0 +1,28 @@
+export function problem (lines) {
+    var a = Number(lines[0])
+    var b = Number(lines[1])
+    var x = a + b
+    console.log(`X = ${x}`)
+}
+
+
+/*
+export function problem (lines) {
+    const a = 2
+    const b = 3
+    var x = Number(a) + Number(b)
+    console.log(`X = ${x}`)
+}
+*/
+
+
+/*
+export function problem (lines) {
+    var a = Number(lines[0])
+    var b = Number(lines[1])
+    var x = a + b
+    console.log(`X = ${x}`)
+}
+*/
+
+
